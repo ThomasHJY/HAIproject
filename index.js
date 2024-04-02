65a1924be4e1db85b6f7266cd4cda1fd033af81d
Thomas <jythomas@naver.com>
2024-04-02 19:18:23 +0900

    빌드 코드

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import SchoolMapScreen from './App.js';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => SchoolMapScreen);
