b18f6b891c80a95696b09f20a462f9400eb4beaf
Thomas <jythomas@naver.com>
2024-04-02 18:31:33 +0900

    9번 건물

---
+++ source/screen/building09.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Image, ScrollView, TouchableOpacity} from 'react-native';
+
+const Building09 = ({navigation}) => {
+    return (
+        <ScrollView>
+            
+        </ScrollView>
+    );
+  };
+  
+  export default Building09;
