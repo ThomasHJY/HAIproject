f43dfb5eec5cc8b0e74e6886c06887cc335e47bc
Thomas <jythomas@naver.com>
2024-04-03 21:53:54 +0900

    9번 건물

---
+++ source/screen/building09.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+
+const Home = ({navigation}) => {
+    return (
+        <ScrollView>
+        </ScrollView>
+    );
+  };
+  
+  export default Home;
+
+  const style = StyleSheet.create({
+    
+  });
