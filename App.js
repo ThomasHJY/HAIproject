28cdfed7fc0bf75a6b1819b8364ab2d25684e0b0
Thomas <jythomas@naver.com>
2024-04-03 21:53:45 +0900

    메인 코드

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import Home from './source/screen/home'; //홈페이지
+import Building09 from './source/screen/building09';
+
 const Stack = createNativeStackNavigator();
 
 const SchoolMapScreen = () => {
@@ -12,6 +15,11 @@ const SchoolMapScreen = () => {
           name = 'Home'
           component = {Home}
           options = {{title: 'Homepage'}}
+        />
+        <Stack.Screen
+          name = 'Building09'
+          component = {Building09}
+          options = {{title: 'College of Engineering'}}
         />
        </Stack.Navigator>
     </NavigationContainer>
