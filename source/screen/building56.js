6da698ad44c8059bb55f315ab038f053be800428
Thomas <jythomas@naver.com>
2024-04-06 05:22:48 +0900

    56주년 기념관 코드

---
+++ source/screen/building56.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, ScrollView, StyleSheet} from 'react-native';
+
+const Building56 = ({navigation}) => {
+    return (
+        <ScrollView>
+            <Text>56주년 기념관 페이지</Text>
+        </ScrollView>
+    );
+  };
+  
+  export default Building56;
+
+  const style = StyleSheet.create({
+    
+  });
