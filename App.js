29a5484e2497e064b78315fd69a6d619bfac4c2a
Thomas <jythomas@naver.com>
2024-04-06 05:22:29 +0900

    메인 코드

--- App.js
+++ App.js
@@ -4,22 +4,28 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from './source/screen/home'; //홈페이지
 import Building09 from './source/screen/building09'; //9번 건물
+import Building56 from './source/screen/building56'; //56번 건물
 
 const Stack = createNativeStackNavigator();
 
 const SchoolMapScreen = () => {
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName = 'Home'>
+       <Stack.Navigator initialRouteName = 'Homepage'>
         <Stack.Screen
-          name = 'Home'
+          name = 'Homepage'
           component = {Home}
-          options = {{title: 'Homepage'}}
+          options = {{title: '홈페이지'}}
         />
         <Stack.Screen
-          name = 'Building09'
+          name = 'College of Engineering'
           component = {Building09}
-          options = {{title: 'College of Engineering'}}
+          options = {{title: '공과대학'}}
+        />
+        <Stack.Screen
+          name = '56th Anniversary Memorial Hall'
+          component = {Building56}
+          options = {{title: '56주년 기념관(법정대학, 특수대학원)'}}
         />
        </Stack.Navigator>
     </NavigationContainer>
