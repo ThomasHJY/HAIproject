d6b3674b2db819a9412d4752d6476fb995a13ffa
Thomas <jythomas@naver.com>
2024-04-16 22:09:13 +0900

    9번 건물

--- source/screen/building09.js
+++ source/screen/building09.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {Text, ScrollView, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import Bottombar from '../component/bottomBar'; //하단 버튼 바
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const Building09 = ({navigation}) => {
     return (
-        <ScrollView>
-            <Text>공과대학 페이지</Text>
-        </ScrollView>
+        <SafeAreaProvider>
+            <SafeAreaView style = {{width: windowWidth, height: windowHeight * 0.9, justifyContent: 'space-between'}}>
+                <View style = {{flex: 1}}>
+                    <Text>공과대학 페이지</Text>
+                </View>
+                <Bottombar n = {navigation}/>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
   };
   
