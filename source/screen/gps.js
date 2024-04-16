ddcf108515e93dec574622d74b82c6cd0b783a6d
Thomas <jythomas@naver.com>
2024-04-16 22:08:48 +0900

    내비게이션 페이지

---
+++ source/screen/gps.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import Bottombar from '../component/bottomBar'; //하단 버튼 바
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const Gps = ({navigation}) => {
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style = {{width: windowWidth, height: windowHeight * 0.9, justifyContent: 'space-between'}}>
+                <View style = {{flex: 1}}>
+                    <Text>내비게이션 페이지</Text>
+                </View>
+                <Bottombar n = {navigation}/>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+  };
+  
+  export default Gps;
+
+  const style = StyleSheet.create({
+    
+  });
