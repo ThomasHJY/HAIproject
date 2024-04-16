e0f8515402d06a9cafdf1dcc98db823d1bd6af31
Thomas <jythomas@naver.com>
2024-04-16 22:09:26 +0900

    56번 건물

--- source/screen/building56.js
+++ source/screen/building56.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {Text, ScrollView, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import Bottombar from '../component/bottomBar'; //하단 버튼 바
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const Building56 = ({navigation}) => {
     return (
-        <ScrollView>
-            <Text>56주년 기념관 페이지</Text>
-        </ScrollView>
+        <SafeAreaProvider>
+            <SafeAreaView style = {{width: windowWidth, height: windowHeight * 0.9, justifyContent: 'space-between'}}>
+                <View style = {{flex: 1}}>
+                    <Text>56주년 기념관 페이지</Text>
+                </View>
+                <Bottombar n = {navigation}/>
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
   };
-  
+
   export default Building56;
 
   const style = StyleSheet.create({
