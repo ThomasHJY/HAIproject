876d3b7a04bde46ad85a582cf1e406990728201d
Thomas <jythomas@naver.com>
2024-05-13 01:43:55 +0900

    공지사항

--- source/screen/notice.js
+++ source/screen/notice.js
@@ -10,7 +10,7 @@ const windowHeight = Dimensions.get('window').height;
 const Notice = ({navigation}) => {
     return (
         <SafeAreaProvider>
-            <SafeAreaView style = {{width: windowWidth, height: windowHeight * 0.9, justifyContent: 'space-between'}}>
+            <SafeAreaView style = {style.container}>
                 <View style = {{flex: 1}}>
                     <Text>공지 사항 페이지</Text>
                 </View>
@@ -23,5 +23,9 @@ const Notice = ({navigation}) => {
   export default Notice;
 
   const style = StyleSheet.create({
-    
+    container: {
+        width: windowWidth, 
+        height: windowHeight, 
+        justifyContent: 'space-between',
+    },
   });
