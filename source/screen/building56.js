2d4ee414b5b7c5609e394d4a14585aa6ee400ff6
Thomas <jythomas@naver.com>
2024-05-13 00:51:09 +0900

    56주년기념관

--- source/screen/building56.js
+++ source/screen/building56.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, ScrollView, StyleSheet, TouchableOpacity, Dimensions} from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import Bottombar from '../component/bottomBar'; //하단 버튼 바
@@ -10,10 +10,37 @@ const windowHeight = Dimensions.get('window').height;
 const Building56 = ({navigation}) => {
     return (
         <SafeAreaProvider>
-            <SafeAreaView style = {{width: windowWidth, height: windowHeight * 0.925, justifyContent: 'space-between'}}>
-                <View style = {{flex: 1}}>
-                    <Text>56주년 기념관 페이지</Text>
-                </View>
+            <SafeAreaView style = {{width: windowWidth, height: windowHeight * 0.9, justifyContent: 'space-between'}}>
+            <ScrollView style = {{flex: 1, backgroundColor: '#ffffff'}} contentContainerStyle = {style.container}>
+                    <TouchableOpacity
+                        style = {style.floor}
+                    >
+                        <View style = {style.floorNumber}>
+                            <Text style = {style.numberText}>
+                                1F
+                            </Text>
+                        </View>
+                        <View style = {style.floorInfo}>
+                            <Text style = {style.infoText}>
+                                ?
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style = {style.floor}
+                    >
+                        <View style = {style.floorNumber}>
+                            <Text style = {style.numberText}>
+                                2F
+                            </Text>
+                        </View>
+                        <View style = {style.floorInfo}>
+                            <Text style = {style.infoText}>
+                                ??
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </ScrollView>
                 <Bottombar n = {navigation}/>
             </SafeAreaView>
         </SafeAreaProvider>
@@ -23,5 +50,40 @@ const Building56 = ({navigation}) => {
   export default Building56;
 
   const style = StyleSheet.create({
-    
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        },
+    floor: {
+        width: windowWidth,
+        height: windowHeight * 0.1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        gap: 10,
+    },
+    floorNumber: {
+        flex: 1.5,
+        height: '75%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#deb887',
+    },
+    floorInfo: {
+        flex: 8.5,
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        backgroundColor: '#ffffff',
+    },
+    numberText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#a52a2a',
+    },
+    infoText: {
+        fontSize: 15,
+        color: '#000000',
+    },
   });
